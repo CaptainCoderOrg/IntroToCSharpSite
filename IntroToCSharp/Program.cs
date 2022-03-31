cffe8c99ab505226621058aeba6a6fc1e7af49be
Joseph Collard <jcollard@chadwickschool.org>
2022-03-31 12:52:13 -0700

    chore: Add Radzen to Program and MainLayout

--- IntroToCSharp/Program.cs
+++ IntroToCSharp/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Radzen;
 
 namespace IntroToCSharp
 {
@@ -18,6 +19,10 @@ namespace IntroToCSharp
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
 
             await builder.Build().RunAsync();
         }
