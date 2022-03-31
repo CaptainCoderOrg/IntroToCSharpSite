979d78c77bdfa78c89fd38c64f3036cfa166d7d4
jcollard <jcollard@users.noreply.github.com>
2022-03-31 19:55:17 +0000

    Deploying to gh-pages from  @ 78a122fa99fabfd33a247a8556f72ebcd4b019bb 🚀

---
+++ js/ReplIt.js
@@ -0,0 +1,8 @@
+/**
+ * Reloads the specified IFrame
+ * @param {string} id The id of the iframe to reload
+ */
+function ReloadIFrame(id) {
+    let iframe = document.getElementById(id);
+    iframe.src += '';
+}
