78a122fa99fabfd33a247a8556f72ebcd4b019bb
Joseph Collard <jcollard@chadwickschool.org>
2022-03-31 12:53:37 -0700

    feat: Add refresh button to ReplIt iframe.

--- IntroToCSharp/Shared/Components/ReplIt/ReplIt.razor.cs
+++ IntroToCSharp/Shared/Components/ReplIt/ReplIt.razor.cs
@@ -1,6 +1,9 @@
 namespace CaptainCoder
 {
     using Microsoft.AspNetCore.Components;
+    using Microsoft.JSInterop;
+    using Radzen;
+
     public class ReplIt : ComponentBase
     {
         [Parameter, EditorRequired]
@@ -8,8 +11,18 @@ namespace CaptainCoder
 
         [Parameter]
         public bool OutputOnly { get; set; } = true;
+
+        [Inject]
+        public TooltipService TooltipService { get; set; } = null!;
+        
+        [Inject]
+        private IJSRuntime JS { get; set; } = null!;
         public string Url => $"https://replit.com/@{Path}?{Options}";
         private string Options => "lite=true" + (OutputOnly ? "&outputonly=true" : "");
-        
+        public void ShowTooltip(ElementReference elementReference, string message) => TooltipService.Open(elementReference, message, new TooltipOptions(){ Duration = 3000, Position = TooltipPosition.Right});
+        public async void ReloadIFrame()
+        {
+            await JS.InvokeVoidAsync("ReloadIFrame", Path);
+        }
     }
 }
