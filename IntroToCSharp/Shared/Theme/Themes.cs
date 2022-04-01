e9afd3b9681f1552599dd86858577b7f2e00618a
Joseph Collard <jcollard@chadwickschool.org>
2022-04-01 09:46:52 -0700

    feat: Add nav menu.

---
+++ IntroToCSharp/Shared/Theme/Themes.cs
@@ -0,0 +1,24 @@
+using MudBlazor;
+
+public class Themes
+{
+    public static readonly MudTheme s_CSharpTheme = new MudTheme()
+    {
+        // Palette = new Palette()
+        // {
+        //     Primary = Colors.Blue.Default,
+        //     Secondary = Colors.Green.Accent4,
+        //     AppbarBackground = Colors.Red.Default,
+        // },
+        // PaletteDark = new Palette()
+        // {
+        //     Primary = Colors.Blue.Lighten1
+        // },
+
+        // LayoutProperties = new LayoutProperties()
+        // {
+        //     DrawerWidthLeft = "260px",
+        //     DrawerWidthRight = "300px"
+        // }
+    };
+}
