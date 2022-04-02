730f4c67fee227328a043c434a31a5098650b619
Joseph Collard <jcollard@chadwickschool.org>
2022-04-02 08:40:57 -0700

    fix: Add fix to reload ReplIt IFrame. This is a little hack that invokes a reload of the IFrame after there is enough space to render it. A better fix would be to wait until the space is available before loading the IFrame at all.

--- IntroToCSharp/Shared/Components/ReplIt/ReplIt.razor.cs
+++ IntroToCSharp/Shared/Components/ReplIt/ReplIt.razor.cs
@@ -19,9 +19,11 @@ namespace CaptainCoder
         private IJSRuntime JS { get; set; } = null!;
         public string Url => $"https://replit.com/@{Path}?{Options}";
         private string Options => "lite=true" + (OutputOnly ? "&outputonly=true" : "");
-        public async void ReloadIFrame()
+        public async Task ReloadIFrame()
         {
             await JS.InvokeVoidAsync("ReloadIFrame", Path);
         }
+
+        protected override async void OnAfterRender(bool firstRender) => await ReloadIFrame();
     }
 }
