741823242434210fb15f1746d54d5427809cdb71
Joseph Collard <jcollard@chadwickschool.org>
2022-04-03 08:47:44 -0700

    docs: Add comments to NotificationService.

--- IntroToCSharp/Shared/Service/NotificationService.cs
+++ IntroToCSharp/Shared/Service/NotificationService.cs
@@ -1,9 +1,21 @@
 using MudBlazor;
 
+/// <summary>
+/// The NotificationService is a singleton that is used throughout
+/// the application to display Snackbar messages to the user.
+/// </summary>
 public class NotificationService
 {
+    /// <summary>
+    /// Gets the NotificationService
+    /// </summary>
     public static NotificationService Service { get; } = new NotificationService();
 
+    /// <summary>
+    /// This method should be called once during initialization of the application
+    /// to specify the ISnackbar to use for displaying messages.
+    /// </summary>
+    /// <param name="snackbar">The Snackbar to use for displaying messages.</param>
     public static void Init(ISnackbar snackbar)
     {
         if (snackbar == null) throw new ArgumentNullException("Snackbar must not be null.");
@@ -12,6 +24,11 @@ public class NotificationService
     }
 
     private ISnackbar? _snackbar;
+
+    /// <summary>
+    /// Waits for the Snackbar service to be injected before returning it.
+    /// </summary>
+    /// <returns>The Snackbar service</returns>
     public async Task<ISnackbar> GetSnackbar()
     {
         await Task.Run(() => { while (_snackbar == null ) Thread.Sleep(100); });
@@ -21,9 +38,14 @@ public class NotificationService
     private NotificationService() {}
 
     /* Start: Delegate Methods for ISnackbar */
+    /// <summary>
+    /// A convenience method for adding messages to the underlying Snackbar.
+    /// </summary>2
     public async Task<Snackbar> Add(string message, Severity severity = Severity.Normal, Action<SnackbarOptions> configure = null!) => (await GetSnackbar()).Add(message, severity, configure);
+
+    /// <summary>
+    /// A convenience method for clearing messages from the underlying Snackbar.
+    /// </summary>2
     public async void Clear() => (await GetSnackbar()).Clear();
-    public async void Remove(Snackbar snackbar) => (await GetSnackbar()).Remove(snackbar);
-    public async void Dispose() => (await GetSnackbar()).Dispose();
     /* End: Delegate Methods for ISnackbar */
 }
