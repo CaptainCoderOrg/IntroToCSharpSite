ba38939a9fa09653e2c5415f77bb36ec5c1748ea
Joseph Collard <jcollard@chadwickschool.org>
2022-04-04 20:22:42 -0700

    feat: Add rendering hack to ReplIt IFrame. ReplIt IFrame console didn't have scroll bars. Resizing after it loads solves the issue. The timeout is set to 1 second.

--- IntroToCSharp/Shared/Components/ReplIt/ReplIt.razor.cs
+++ IntroToCSharp/Shared/Components/ReplIt/ReplIt.razor.cs
@@ -16,14 +16,26 @@ namespace CaptainCoder
         public bool ShareSource { get; set; } = false;
         [Parameter]
         public string MinHeight { get; set; } = "250px";
-                
+        public string ResizeHeight { get; private set; } = "0px";
+
         [Inject]
         private IJSRuntime JS { get; set; } = null!;
         public string Url => $"https://replit.com/@{Path}?{Options}";
         private string Options => "lite=true" + (OutputOnly ? "&outputonly=true" : "");
         public async Task ReloadIFrame()
         {
+            ResizeHeight = "0px";
             await JS.InvokeVoidAsync("ReloadIFrame", Path);
+        }
+
+        public async void ResizeFrame()
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                ResizeHeight = MinHeight;
+                StateHasChanged();
+            });
         }
     }
 }
