065093829d900dc06cc5e2e0977cc9c5fb6fdc32
Joseph Collard <jcollard@chadwickschool.org>
2022-04-05 20:17:23 -0700

    feat: Add rudimentary console popup.

--- IntroToCSharp/Shared/Components/ReplIt/Nix.razor.cs
+++ IntroToCSharp/Shared/Components/ReplIt/Nix.razor.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Components;
+
 namespace IntroToCSharp.Shared.Components.ReplIt
 {
 
     public sealed partial class Nix
     {
+        [Parameter]
         public string MinHeight { get; set; } = "500px";
         public string ResizeHeight { get; private set; } = "0px";
         public string Url = "https://replit.com/@JosephCollard/C-Project?lite=true";
