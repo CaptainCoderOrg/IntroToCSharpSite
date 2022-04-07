2482a74c3b5a986e276a9072e897c7879c84650b
Joseph Collard <jcollard@chadwickschool.org>
2022-04-07 12:28:15 -0700

    wip: Add basic ConfigPanel to Nix.

---
+++ IntroToCSharp/Shared/Components/Nix/Nix.razor.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace IntroToCSharp.Shared.Components.Nix
+{
+
+    public sealed partial class Nix
+    {
+        private Dictionary<string, string> Projects = new Dictionary<string, string>();
+        private static int s_NEXT_ID = 0;
+        private string? _url = "https://replit.com/@JosephCollard/C-Project?lite=true";
+        private string Url
+        {
+            get => _url == null ? "" : _url;
+            set
+            {
+                _url = value;
+                StateHasChanged();
+            }
+        }
+        private int _id = s_NEXT_ID++;
+        [Inject]
+        IDialogService DialogService { get; set; }
+        [Inject]
+        internal IJSRuntime JS { get; set; }
+        [Parameter]
+        public string MinHeight { get; set; } = "500px";
+        [Parameter]
+        public Action? OnClose { get; set; } = null;
+        public string ResizeHeight { get; private set; } = "0px";
+        public string ID => $"nix-{_id}";
+        protected override void OnInitialized()
+        {
+            Projects["Project 1"] = "https://replit.com/@JosephCollard/C-Project?lite=true";
+            Projects["Project 2"] = "https://replit.com/@JosephCollard/AskName?lite=true";
+            OpenConfig();
+        }
+        private async void ResizeFrame()
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                ResizeHeight = MinHeight;
+                StateHasChanged();
+            });
+        }
+
+        private async Task ReloadIFrame()
+        {
+            ResizeHeight = "0px";
+            await JS.InvokeVoidAsync("ReloadIFrame", ID);
+        }
+
+        private void OpenConfig()
+        {
+            DialogService.Show<ConfigPanel>();
+        }
+
+        private void ChangeProject(ChangeEventArgs e)
+        {
+            Url = e.Value?.ToString()!;
+        }
+    }
+}
