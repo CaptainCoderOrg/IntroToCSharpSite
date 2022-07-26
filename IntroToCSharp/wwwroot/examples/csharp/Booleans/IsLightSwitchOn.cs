92cb41f2c56b71c1ccec1d0d961b40900f893d4e
Ryan Rosario <ryanrosario@comcast.net>
2022-07-26 15:57:54 -0400

    Rough draft of Booleans completed

---
+++ IntroToCSharp/wwwroot/examples/csharp/Booleans/IsLightSwitchOn.cs
@@ -0,0 +1,13 @@
+using System;
+
+    //Here we are asking if the light switch on and setting it to false!
+    bool IsLightSwitchOn = false;
+
+    if(IsLightSwitchOn == true)
+    {
+      Console.WriteLine("The light switch is on! Wow it is bright!");
+    }
+    else(IsLightSwitchOn == false)
+    {
+      Console.WriteLine("The light switch is off! I can't see a thing!");
+    }
