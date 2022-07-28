dee7f76b1c52ce5e9d23c1f80e6139cdbe26b775
Carter Warren <cwarren2023@chadwickschool.org>
2022-07-28 10:38:21 -0700

    feat: Variables lesson

--- IntroToCSharp/Shared/Components/Keyword/KeywordRegistry.cs
+++ IntroToCSharp/Shared/Components/Keyword/KeywordRegistry.cs
@@ -14,8 +14,8 @@ namespace CaptainCoder {
 
         static KeywordRegistry(){
             INSTANCE._registry["command prompt"] = "The area where you type in your console is called the command prompt.";
-            INSTANCE._registry["lowerCamelCase"] = "each word is capitalized except for the first";
-            INSTANCE._registry["UpperCamelCase"] = "each word is capitalized";
+            INSTANCE._registry["lowercamelcase"] = "Each word is capitalized except for the first (Ex: lowerCamelCase)";
+            INSTANCE._registry["uppercamelcase"] = "Each word is capitalized (Ex: UpperCamelCase)";
         }
 
         /// <summary>
