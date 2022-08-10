143389766dc982e9127afc83456a874f1d9ef0d5
Carter Warren <cwarren2023@chadwickschool.org>
2022-08-10 12:18:41 -0600

    feat: Add basic functioning of sorting system

--- IntroToCSharp/Shared/Components/Items/MockDatabase.cs
+++ IntroToCSharp/Shared/Components/Items/MockDatabase.cs
@@ -8,24 +8,23 @@ public class MockDatabase : IItemDatabase
 
     public MockDatabase()
     {
-        // AllItems.Add();
-        AllItems.Add(new("Axe", "Weapon", 1000, "A simple, but effective axe.",
+        AllItems.Add(new("Axe", "Weapons", 1000, "A simple, but effective axe.",
         "/imgs/store/weapons/axe.png"));
-        AllItems.Add(new("Iron Sword", "Weapon", 1500, "A durable sword made of iron.",
+        AllItems.Add(new("Iron Sword", "Weapons", 1500, "A durable sword made of iron.",
             "/imgs/store/weapons/IronSword.png"));
-        AllItems.Add(new("Golden Sword", "Weapon", 30000000, "A strong and fast sword, but breaks easily.",
+        AllItems.Add(new("Golden Sword", "Weapons", 30000, "A strong and fast sword, but breaks easily.",
             "/imgs/store/weapons/GoldSword.png"));
-        AllItems.Add(new ("Golden Axe", "Weapon", 3000, "A very heavy hitting axe, but not super durable.",
+        AllItems.Add(new("Golden Axe", "Armor", 3000, "A very heavy hitting axe, but not super durable.",
             "/imgs/store/weapons/GoldAxe.png"));
     }
 
     public List<StoreItem> GetItems(string filter)
     {
+        Console.WriteLine($"Getting Items with filter: {filter}");
         if (filter == "All")
         {
             return AllItems;
         }
-        return null;
-        // return AllItems.Where(???).ToList();
+        return AllItems.Where(item => item.Type == filter).ToList();
     }
 }
