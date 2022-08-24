838f8275b780ddf8aca2424b650acb61aa915e26
Ryan Rosario <ryanrosario@comcast.net>
2022-08-24 11:42:46 -0400

    Created Shape Analyzer Activity Rough Draft. Includes: Perimeter of a Rectangle, Area of a Rectangle, Area of A Circle, How Fast is the Flash. Also added Bbeg picture.

--- IntroToCSharp/Shared/Components/Engage/Adventure/Portrait.cs
+++ IntroToCSharp/Shared/Components/Engage/Adventure/Portrait.cs
@@ -2,4 +2,5 @@ namespace CaptainCoder;
 public readonly record struct Portrait(string Name, string Src);
 public static class Portraits {
     public static readonly Portrait CaptainCoder = new ("Captain Coder", "captain-coder.png");
+    public static readonly Portrait Bbeg = new ("Bbeg", "bbeg.jpg");
 }
