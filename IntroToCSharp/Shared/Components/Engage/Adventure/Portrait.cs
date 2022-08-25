dea9649beab699b11adef65d8b81d89e1f1c16a3
Joseph Collard <jcollard@users.noreply.github.com>
2022-08-25 05:36:54 -0700

    chore: Add Guard Portrait

--- IntroToCSharp/Shared/Components/Engage/Adventure/Portrait.cs
+++ IntroToCSharp/Shared/Components/Engage/Adventure/Portrait.cs
@@ -4,4 +4,5 @@ public static class Portraits {
     public static readonly Portrait CaptainCoder = new ("Captain Coder", "captain-coder.png");
     public static readonly Portrait Bbeg = new ("Bbeg", "bbeg.jpg");
     public static readonly Portrait Nefaria = new ("Nefaria", "nefaria.png");
+    public static readonly Portrait Guard = new ("Guard", "guard.png");
 }
