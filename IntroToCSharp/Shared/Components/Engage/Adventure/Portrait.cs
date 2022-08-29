ba9445c2230c5a033d2af0cf11cc851f7a0cef2c
Ryan Rosario <ryanrosario@comcast.net>
2022-08-29 12:21:25 -0400

    Issue: Complete Setting Builder Activity Completed. Task: Added Ryan Portrait

--- IntroToCSharp/Shared/Components/Engage/Adventure/Portrait.cs
+++ IntroToCSharp/Shared/Components/Engage/Adventure/Portrait.cs
@@ -2,4 +2,5 @@ namespace CaptainCoder;
 public readonly record struct Portrait(string Name, string Src);
 public static class Portraits {
     public static readonly Portrait CaptainCoder = new ("Captain Coder", "captain-coder.png");
+    public static readonly Portrait Ryan_1 = new ("Ryan", "ryan_1.png");
 }
