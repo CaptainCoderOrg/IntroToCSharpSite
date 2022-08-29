12e58a1ce1b83dba2a1c7f318dadb378beac3cff
Ryan Rosario <ryanrosario@comcast.net>
2022-08-29 18:58:53 -0400

    fix: Fix login bug with GitHub OAuth. Adds error message in Snack Bar upon failure for all logins.

--- IntroToCSharp/Shared/User/User.cs
+++ IntroToCSharp/Shared/User/User.cs
@@ -1,5 +1,6 @@
 using CaptainCoder;
 using System.Text.Json;
+using MudBlazor;
 
 namespace CaptainCoder;
 /// <summary>
@@ -106,13 +107,21 @@ public abstract class User
         if (jsonDocument.RootElement.TryGetProperty("providerData", out JsonElement providerData))
         {
             string providerId = providerData[0].GetProperty("providerId").GetString()!;
-            return providerId switch
+            try 
             {
-                "password" => new PasswordUser(jsonDocument),
-                "google.com" => new GoogleUser(jsonDocument),
-                "github.com" => new GitHubUser(jsonDocument),
-                _ => throw new Exception($"Invalid providerID: {providerId}")
-            };
+                return providerId switch
+                {
+                    "password" => new PasswordUser(jsonDocument),
+                    "google.com" => new GoogleUser(jsonDocument),
+                    "github.com" => new GitHubUser(jsonDocument),
+                    _ => throw new Exception($"Invalid providerID: {providerId}")
+                };
+            } 
+            catch
+            {
+                NotificationService.Service.Add("An error occurred while logging in.", MudBlazor.Severity.Error).AndForget();
+                UserService.Service.Logout().AndForget();
+            }
         }
         return new NoUser();
     }
