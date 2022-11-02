d5f84fdd5a1e2120a3b3efb4d0d1466c625bfb97
jcollard <josephmcollard@gmail.com>
2022-11-02 06:13:20 -0700

    feat: Add While Loops section (#213)
    Co-authored-by: Joseph Collard <jcollard@users.noreply.github.com>

--- IntroToCSharp/Shared/Components/Engage/Adventure/Portrait.cs
+++ IntroToCSharp/Shared/Components/Engage/Adventure/Portrait.cs
@@ -5,4 +5,5 @@ public static class Portraits {
     public static readonly Portrait Bbeg = new ("Bbeg", "bbeg.jpg");
     public static readonly Portrait Nefaria = new ("Nefaria", "nefaria.png");
     public static readonly Portrait Guard = new ("Guard", "guard.png");
+    public static readonly Portrait Lucy = new ("Lucy", "lucy.png");
 }
